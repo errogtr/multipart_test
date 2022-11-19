be7e0eafd633a619404a99c1f6a1b6338bbf5c42
enrico <enrico.dsf@gmail.com>
2022-11-20 00:49:28 +0100

    changed response format

--- api/routes.py
+++ api/routes.py
@@ -4,12 +4,12 @@ from flask import current_app, request, make_response
 @current_app.route("/process", methods=["POST"])
 def process_request_api():
     attachment = request.files.get("attachment")
+    content = attachment.stream.read().decode()
     headers = attachment.headers
     try:
         result = next({k: v} for k, v in headers.items() if k == "ID")
-        status_code = 200
+        response = make_response(f"STATUS 200 result {result}\n")
     except StopIteration:
-        result = {"ID": "not found"}
-        status_code = 9999
-    result.update(status=status_code)
-    return make_response(result)
+        status = 9999
+        response = make_response(f"STATUS {status} ID not found\n", status)
+    return response
