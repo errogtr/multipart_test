b8f7f1da7ee28c334fd7dd64809b2b49b07089c4
enrico <enrico.dsf@gmail.com>
2022-11-20 00:55:32 +0100

    removed unused variable

--- api/routes.py
+++ api/routes.py
@@ -4,7 +4,6 @@ from flask import current_app, request, make_response
 @current_app.route("/process", methods=["POST"])
 def process_request_api():
     attachment = request.files.get("attachment")
-    content = attachment.stream.read().decode()
     headers = attachment.headers
     try:
         result = next({k: v} for k, v in headers.items() if k == "ID")
